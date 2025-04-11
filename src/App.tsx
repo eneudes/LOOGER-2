74e73716c38dd8f761ebd6bd82e9c6f44aeca34f
eneudes <eneudes.ap@gmail.com>
2025-04-11 15:15:12 -0300

    projeto finalizado

--- src/App.tsx
+++ src/App.tsx
@@ -19,8 +19,8 @@ export function App() {
     setSearchResults(filteredResults)
   }
   return (
-    <div className=" max-md:bg-amber-200 max-md:w-full w-full flex-col items-center justify-center px-10 max-md:px-2 bg-zinc-300">
-      <div className="max-md:flex-col max-md:w-full m-auto w-5xl flex py-12 items-center">
+    <div className=" max-md:w-full w-full flex-col items-center justify-center px-10 max-md:px-2 ">
+      <div className="max-md:flex-col max-md:gap-5 max-md:justify-center max-md:items-center max-md:w-full m-auto w-5xl flex py-12 items-center">
         <h1 className="pr-16 text-3xl">LOOGER</h1>
         <SearchBar onSearch={handleSearch} />
       </div>
--- src/components/SearchResults.tsx
+++ src/components/SearchResults.tsx
@@ -11,11 +11,11 @@ export function SearchResults({ results }: SearchResultsProps) {
     return (
       <>
         {results.map((item) => (
-          <div key={item.id} className="max-md:w-full m-auto w-5xl bg-zinc-800 border border-zinc-700 rounded-sm my-1 flex-col gap-12 p-5">
+          <div key={item.id} className="max-md:w-full m-auto w-5xl bg-zinc-800 border border-zinc-700 rounded-sm my-1 flex-col p-1 items-center justify-center">
             <h1 className='max-md:w-full bg-zinc-900 w-56 flex justify-center'>{item.servidor}</h1>
-            <div><span className='max-md:w-full text-zinc-100'>PASTA :</span><span className='text-zinc-500 font-bold max-md:text-[5px]'> {item.pasta}</span></div>
-            <div><span className='text-zinc-100'>PALAVRA CHAVE : </span><span className='text-sky-500 font-normal text-sm'>{item.descricao}</span></div>
-            <div className='w-full bg-zinc-900 p-1'><span className='text-zinc-300 font-normal text-[10px] max-md:text-[5px]'>{item.local}</span></div>
+            <div><p className='text-zinc-500 font-normal max-md:text-[9px] max-md:break-words'> <span className=' max-md:text-[9px] text-zinc-100'>PASTA : </span>{item.pasta}</p></div>
+            <div><p className='text-sky-500 font-normal  max-md:text-[9px]'><span className='max-md:text-[9px] text-zinc-100'>PALAVRA CHAVE : </span>{item.descricao}</p></div>
+            <div className='w-full bg-zinc-900 p-1'><p className='text-zinc-300 font-normal text-[10px] max-md:text-[5px] max-md:box-border'>{item.local}</p></div>
           </div>
         ))}
       </>
